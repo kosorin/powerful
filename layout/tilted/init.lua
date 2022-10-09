49f62f8af715e1da474135afbcf6816a79234fec
David Kosorin <david@kosorin.net>
2022-10-09 03:00:38 +0200

    Improve Tilted layout

--- layout/tilted/init.lua
+++ layout/tilted/init.lua
@@ -4,7 +4,7 @@ local capi = {
     screen = screen,
 }
 local math = math
-local select = select
+local infinity = math.huge
 local find = string.find
 local aclient = require("awful.client")
 local amouse = require("awful.mouse")
@@ -30,10 +30,6 @@ tilted.resize_only_neighbors = false
 -- @field powerful.layout.tilted.resize_jump_to_corner
 tilted.resize_jump_to_corner = true
 
---- Minimum client size (excluding `border_width` and `useless_gap`).
--- @field powerful.layout.tilted.minimum_client_size
-tilted.minimum_client_size = 8
-
 local function any_button(buttons)
     for i = 1, #buttons do
         if buttons[i] then
@@ -43,39 +39,32 @@ local function any_button(buttons)
     return false
 end
 
-local function get_titlebar_sizes(client)
+local function get_titlebar_size(client)
     local _, top = client:titlebar_top()
     local _, bottom = client:titlebar_bottom()
     local _, left = client:titlebar_left()
     local _, right = client:titlebar_right()
-    return left, right, top, bottom
+    return {
+        width = left + right,
+        height = top + bottom,
+    }
 end
 
-local function get_geometry(client, useless_gap)
-    local left, right, top, bottom = get_titlebar_sizes(client)
-    local border_width = client.border_width
-    local real_geometry = client:geometry()
-    local geometry = {
-        x = real_geometry.x - useless_gap - border_width,
-        y = real_geometry.y - useless_gap - border_width,
-        width = real_geometry.width + (2 * (border_width + useless_gap)),
-        height = real_geometry.height + (2 * (border_width + useless_gap)),
-    }
-    local min_size = {
-        width = (geometry.width - real_geometry.width) + tilted.minimum_client_size + left + right,
-        height = (geometry.height - real_geometry.height) + tilted.minimum_client_size + top + bottom,
-    }
-    return geometry, min_size
-end
-
-local function apply_size_hints(client, width, height, useless_gap)
-    local left, right, top, bottom = get_titlebar_sizes(client)
+local function get_decoration_size(client, useless_gap)
     local border_width = 2 * (client.border_width + useless_gap)
-    width, height = width - border_width, height - border_width
-    width, height = client:apply_size_hints(
-        math.max(width, tilted.minimum_client_size + top + bottom + border_width),
-        math.max(height, tilted.minimum_client_size + left + right + border_width))
-    return width + border_width, height + border_width
+    local decoration_size = get_titlebar_size(client)
+    decoration_size.width = decoration_size.width + border_width
+    decoration_size.height = decoration_size.height + border_width
+    return decoration_size
+end
+
+local function inflate(geometry, size)
+    return {
+        x = geometry.x - size,
+        y = geometry.y - size,
+        width = geometry.width + (2 * size),
+        height = geometry.height + (2 * size),
+    }
 end
 
 local function get_orientation_info(orientation)
@@ -95,15 +84,14 @@ local function get_orientation_info(orientation)
         _width = "height"
         _height = "width"
     end
-    return _x, _y, _width, _height, is_horizontal, is_reversed, is_reversed_x, is_reversed_y
+    local _fx, _fy = is_horizontal and _x or _y, is_horizontal and _y or _x
+    return _x, _y, _width, _height, is_horizontal, is_reversed, is_reversed_x, is_reversed_y, _fx, _fy
 end
 
 local function resize(orientation, screen, tag, client, corner)
     if not screen or not tag or not client or not client.valid then
         return
     end
-
-    capi.mousegrabber.stop()
 
     local layout_descriptor = tag.tilted_layout_descriptor
     if not layout_descriptor then
@@ -112,13 +100,13 @@ local function resize(orientation, screen, tag, client, corner)
 
     local parameters = alayout.parameters(tag, screen)
 
-    local column_descriptor, index = layout_descriptor:find_client(client, parameters.clients)
+    local column_descriptor, item_descriptor = layout_descriptor:find_client(client, parameters.clients)
     if not column_descriptor then
         return
     end
 
-    local _x, _y, _width, _height, is_horizontal, _, is_reversed_x, is_reversed_y = get_orientation_info(orientation)
-    local _fx, _fy = is_horizontal and _x or _y, is_horizontal and _y or _x
+    local index = item_descriptor.index
+    local _x, _y, _width, _height, is_horizontal, _, is_reversed_x, is_reversed_y, _fx, _fy = get_orientation_info(orientation)
 
     local directions = { x = 0, y = 0 }
     if find(corner, is_reversed_x and "right" or "left", nil, true) then
@@ -156,10 +144,13 @@ local function resize(orientation, screen, tag, client, corner)
         return
     end
 
-    local workarea = parameters.workarea
-    local useless_gap = parameters.useless_gap
-
-    local initial_geometry = get_geometry(client, useless_gap)
+    local initial_geometry = inflate(client:geometry(), client.border_width + parameters.useless_gap)
+    initial_geometry = {
+        x = initial_geometry.x,
+        y = initial_geometry.y,
+        width = (is_horizontal and column_descriptor or item_descriptor).factor * parameters.workarea.width,
+        height = (is_horizontal and item_descriptor or column_descriptor).factor * parameters.workarea.height,
+    }
     local initial_coords = {
         x = initial_geometry.x + ((directions[_x] + 1) * 0.5 * initial_geometry.width),
         y = initial_geometry.y + ((directions[_y] + 1) * 0.5 * initial_geometry.height),
@@ -177,42 +168,176 @@ local function resize(orientation, screen, tag, client, corner)
         }
     end
 
+    layout_descriptor.resize = nil
+
+    capi.mousegrabber.stop()
     capi.mousegrabber.run(function(coords)
-        if client.valid and any_button(coords.buttons) then
-            local current_column_descriptor, current_index = layout_descriptor:find_client(client, aclient.tiled(screen))
-            if column_descriptor ~= current_column_descriptor or index ~= current_index then
-                return false
-            end
-
-            coords.x = coords.x + coords_offset.x
-            coords.y = coords.y + coords_offset.y
-
-            local min_size = select(2, get_geometry(client, useless_gap))
-            local factors = {}
-
-            if directions[_fx] ~= 0 then
-                local value = directions[_fx] < 0
-                    and (initial_geometry[_x] + initial_geometry[_width] - coords[_x])
-                    or (coords[_x] - initial_geometry[_x])
-                factors[_fx] = math.max(min_size[_width], value) / workarea[_width]
-            end
-
-            if directions[_fy] ~= 0 then
-                local value = directions[_fy] < 0
-                    and (initial_geometry[_y] + initial_geometry[_height] - coords[_y])
-                    or (coords[_y] - initial_geometry[_y])
-                factors[_fy] = math.max(min_size[_height], value) / workarea[_height]
-            end
-
-            layout_descriptor:set_factors(column_descriptor, index, factors, original_directions)
-            return true
-        else
+        if not client.valid then
+            layout_descriptor.resize = nil
             return false
         end
+
+        local current_cd, current_id = layout_descriptor:find_client(client, aclient.tiled(screen))
+        if column_descriptor ~= current_cd or item_descriptor ~= current_id then
+            layout_descriptor.resize = nil
+            return false
+        end
+
+        if not any_button(coords.buttons) then
+            if layout_descriptor.resize then
+                layout_descriptor.resize.apply = true
+                alayout.arrange(screen)
+            end
+            return false
+        end
+
+        coords.x = coords.x + coords_offset.x
+        coords.y = coords.y + coords_offset.y
+
+        local size = {}
+
+        if directions[_fx] ~= 0 then
+            local value = directions[_fx] < 0
+                and (initial_geometry[_x] + initial_geometry[_width] - coords[_x])
+                or (coords[_x] - initial_geometry[_x])
+            if value < 1 then
+                value = 1
+            end
+            size[_fx] = value
+        end
+
+        if directions[_fy] ~= 0 then
+            local value = directions[_fy] < 0
+                and (initial_geometry[_y] + initial_geometry[_height] - coords[_y])
+                or (coords[_y] - initial_geometry[_y])
+            if value < 1 then
+                value = 1
+            end
+            size[_fy] = value
+        end
+
+        layout_descriptor.resize = {
+            apply = false,
+            column = column_descriptor.index,
+            item = index - column_descriptor.from + 1,
+            size = size,
+            directions = original_directions,
+        }
+        alayout.arrange(screen)
+        return true
     end, cursor)
 end
 
-local function arrange(parameters, orientation)
+local function fit(items, total_size)
+    local count = #items
+    local new_adjusted
+    local adjusted = {}
+    local factors = {}
+    local available_size = total_size
+    repeat
+        new_adjusted = false
+        local total_factor = 0
+        for i = 1, count do
+            local item = items[i]
+            if not adjusted[i] then
+                if item.size < item.min_size then
+                    available_size = available_size - item.min_size
+                else
+                    if not factors[i] then
+                        factors[i] = item.factor
+                    end
+                    total_factor = total_factor + factors[i]
+                end
+            end
+        end
+        for i = 1, count do
+            local item = items[i]
+            if not adjusted[i] then
+                if item.size < item.min_size then
+                    item.size = item.min_size
+                    adjusted[i] = true
+                    new_adjusted = true
+                else
+                    factors[i] = factors[i] / total_factor
+                    item.size = available_size * factors[i]
+                end
+            end
+        end
+        if #adjusted >= count then
+            break
+        end
+    until not (new_adjusted and #adjusted < count)
+end
+
+local function resize_fit(items, start, direction, new_size, apply)
+    local resize_item = items[start]
+    if resize_item.size == new_size then
+        return
+    end
+
+    local min_size = resize_item.min_size
+    if new_size < min_size then
+        new_size = min_size
+    end
+
+    local full_factor = resize_item.factor
+    local full_size = resize_item.size
+    local total_size = 0
+    local total_min_size = 0
+
+    local new_items = {}
+    local stop = direction < 0 and 1 or #items
+    for i = start + direction, stop, direction do
+        local item = items[i]
+        full_factor = full_factor + item.factor
+        full_size = full_size + item.size
+        total_size = total_size + item.size
+        total_min_size = total_min_size + item.min_size
+        new_items[#new_items + 1] = {
+            old_item = item,
+            size = item.size,
+            min_size = item.min_size,
+        }
+    end
+
+    if new_size > full_size - total_min_size then
+        new_size = full_size - total_min_size
+    end
+
+    local new_total_size = full_size - new_size
+    if new_total_size < total_min_size then
+        new_total_size = total_min_size
+    end
+
+    local new_total_factor = 0
+    for i = 1, #new_items do
+        local new_item = new_items[i]
+        new_item.factor = new_item.size / total_size
+        new_item.size = new_item.factor * new_total_size
+        new_total_factor = new_total_factor + new_item.factor
+    end
+    for i = 1, #new_items do
+        local new_item = new_items[i]
+        new_item.factor = new_item.factor / new_total_factor
+    end
+
+    fit(new_items, new_total_size)
+
+    new_items[0] = {
+        old_item = resize_item,
+        size = new_size,
+    }
+    for i = 0, #new_items do
+        local new_item = new_items[i]
+        new_item.old_item.factor = full_factor * (new_item.size / full_size)
+        new_item.old_item.size = new_item.size
+        if apply then
+            new_item.old_item.descriptor.factor = new_item.old_item.factor
+        end
+    end
+end
+
+local function arrange(orientation, parameters)
     local tag = parameters.tag
     local screen = parameters.screen and capi.screen[parameters.screen]
     if not tag and screen then
@@ -222,57 +347,103 @@ local function arrange(parameters, orientation)
         return
     end
 
+    local _x, _y, _width, _height, _, is_reversed = get_orientation_info(orientation)
+
     local clients = parameters.clients
     local workarea = parameters.workarea
     local useless_gap = parameters.useless_gap
+    local width = workarea[_width]
+    local height = workarea[_height]
 
     local layout_descriptor = tilted_layout_descriptor.update(tag, clients)
 
-    local _x, _y, _width, _height, _, is_reversed = get_orientation_info(orientation)
-
-    local geometries = parameters.geometries
-    local width = workarea[_width]
-    local x = workarea[_x]
-    local offset_x = x
+    local layout_data = {
+        descriptor = layout_descriptor,
+    }
 
     for column = 1, layout_descriptor.size do
         column = is_reversed and (layout_descriptor.size - column + 1) or column
         local column_descriptor = layout_descriptor[column]
-        local column_width = column_descriptor.factor * width
 
-        for index = column_descriptor.from, column_descriptor.to do
+        local column_data = {
+            descriptor = column_descriptor,
+            factor = column_descriptor.factor,
+            size = column_descriptor.factor * width,
+            min_size = 0,
+        }
+        for item = 1, column_descriptor.size do
+            local index = column_descriptor.from + item - 1
+            local item_descriptor = column_descriptor[item]
             local client = clients[index]
+
             local size_hints = client.size_hints
-            local width_hint = size_hints["min_" .. _width] or size_hints["base_" .. _width] or 0
-            column_width = math.max(width_hint, column_width)
+            local decoration_size = get_decoration_size(client, useless_gap)
+            local min_width = decoration_size[_width]
+                + math.max(1, size_hints["min_" .. _width] or size_hints["base_" .. _width] or 0)
+            local min_height = decoration_size[_height]
+                + math.max(1, size_hints["min_" .. _height] or size_hints["base_" .. _height] or 0)
+            local max_width = decoration_size[_width]
+                + (size_hints["max_" .. _width] or infinity)
+            local max_height = decoration_size[_height]
+                + (size_hints["max_" .. _height] or infinity)
+
+            column_data[item] = {
+                descriptor = item_descriptor,
+                factor = item_descriptor.factor,
+                size = item_descriptor.factor * height,
+                min_size = min_height,
+            }
+
+            if column_data.min_size < min_width then
+                column_data.min_size = min_width
+            end
         end
 
-        local available_width = width - (x - offset_x)
-        column_width = math.max(1, math.min(column_width, available_width))
-
-        local actual_column_width = 0
-        local height = workarea[_height]
-        local y = workarea[_y]
-
-        for index = column_descriptor.from, column_descriptor.to do
-            local client = clients[index]
-            local i = index - column_descriptor.from + 1
-
-            local geometry = {}
-            geometry[_width] = column_width
-            geometry[_height] = math.max(1, math.floor(column_descriptor[i].factor * height))
-            geometry[_x] = x
-            geometry[_y] = y
-            geometries[client] = geometry
-
-            geometry.width, geometry.height = apply_size_hints(client, geometry.width, geometry.height, useless_gap)
-
-            actual_column_width = math.max(actual_column_width, geometry[_width])
-            y = y + geometry[_height]
-        end
-        x = x + actual_column_width
+        layout_data[column] = column_data
     end
 
+    fit(layout_data, width)
+    for i = 1, #layout_data do
+        fit(layout_data[i], height)
+    end
+
+    local resize = layout_descriptor.resize
+    if resize then
+        if resize.size.x and resize.directions.x ~= 0 then
+            resize_fit(layout_data, resize.column, resize.directions.x, resize.size.x, resize.apply)
+        end
+        if resize.size.y and resize.directions.y ~= 0 then
+            resize_fit(layout_data[resize.column], resize.item, resize.directions.y, resize.size.y, resize.apply)
+        end
+        if resize.apply then
+            layout_descriptor.resize = nil
+        end
+    end
+
+    local x = workarea[_x]
+    for column = 1, layout_descriptor.size do
+        column = is_reversed and (layout_descriptor.size - column + 1) or column
+        local column_descriptor = layout_descriptor[column]
+        local column_data = layout_data[column]
+        local column_width = column_data.size
+
+        local y = workarea[_y]
+        for item = 1, column_descriptor.size do
+            local index = column_descriptor.from + item - 1
+            local item_data = column_data[item]
+            local item_height = item_data.size
+
+            parameters.geometries[clients[index]] = {
+                [_width] = column_width,
+                [_height] = item_height,
+                [_x] = x,
+                [_y] = y,
+            }
+
+            y = y + item_height
+        end
+        x = x + column_width
+    end
 end
 
 function tilted.skip_gap(tiled_client_count, tag)
@@ -287,8 +458,8 @@ tilted.right.is_tilted = true
 tilted.right.name = "tilted"
 tilted.right.skip_gap = tilted.skip_gap
 
-function tilted.right.arrange(p)
-    return arrange(p, "right")
+function tilted.right.arrange(...)
+    return arrange("right", ...)
 end
 
 function tilted.right.resize(...)
@@ -303,8 +474,8 @@ tilted.left.is_tilted = true
 tilted.left.name = "tiltedleft"
 tilted.left.skip_gap = tilted.skip_gap
 
-function tilted.left.arrange(p)
-    return arrange(p, "left")
+function tilted.left.arrange(...)
+    return arrange("left", ...)
 end
 
 function tilted.left.resize(...)
@@ -319,8 +490,8 @@ tilted.bottom.is_tilted = true
 tilted.bottom.name = "tiltedbottom"
 tilted.bottom.skip_gap = tilted.skip_gap
 
-function tilted.bottom.arrange(p)
-    return arrange(p, "bottom")
+function tilted.bottom.arrange(...)
+    return arrange("bottom", ...)
 end
 
 function tilted.bottom.resize(...)
@@ -335,8 +506,8 @@ tilted.top.is_tilted = true
 tilted.top.name = "tiltedtop"
 tilted.top.skip_gap = tilted.skip_gap
 
-function tilted.top.arrange(p)
-    return arrange(p, "top")
+function tilted.top.arrange(...)
+    return arrange("top", ...)
 end
 
 function tilted.top.resize(...)
@@ -349,13 +520,13 @@ tilted.arrange = tilted.right.arrange
 tilted.resize = tilted.right.resize
 
 amouse.resize.add_enter_callback(function(client, args)
+    if client.floating then
+        return
+    end
     local screen = client.screen
     local tag = screen.selected_tag
     local layout = tag and tag.layout or nil
     if layout and layout.is_tilted then
-        if client.floating then
-            return
-        end
         if layout.resize then
             layout.resize(screen, tag, client, args.corner)
         end
--- layout/tilted/layout_descriptor.lua
+++ layout/tilted/layout_descriptor.lua
@@ -13,7 +13,6 @@ function layout_descriptor.object:find_client(client, clients)
         return
     end
 
-    local column_descriptor = nil
     local index = nil
 
     for i = 1, #clients do
@@ -27,98 +26,40 @@ function layout_descriptor.object:find_client(client, clients)
         for i = 1, self.size do
             local cd = self[i]
             if cd.from <= index and index <= cd.to then
-                if cd[index - cd.from + 1].window == client.window then
-                    column_descriptor = cd
+                local id = cd[index - cd.from + 1]
+                if id.index == index and id.window == client.window then
+                    return cd, id
                 end
                 break
             end
         end
     end
-
-    if column_descriptor then
-        return column_descriptor, index
-    end
 end
 
-local function clamp(value, min, max)
-    if value < min then return min end
-    if value > max then return max end
-    return value
-end
-
-local function set_factor(descriptor, index, factor, direction)
-    assert(direction ~= 0)
-
-    local start = index - descriptor.from + 1
-
-    if layout_descriptor.tilted.resize_only_neighbors then
-        local neighbor = clamp(start + direction, 1, descriptor.size)
-        if start ~= neighbor then
-            local total = descriptor[start].factor + descriptor[neighbor].factor
-            descriptor[start].factor = clamp(factor, 0.01, total - 0.01)
-            descriptor[neighbor].factor = total - descriptor[start].factor
-        end
-    else
-        descriptor[start].factor = clamp(factor, 0.01, 0.99)
-
-        local stop = direction < 0 and 1 or descriptor.size
-        local reverse_stop = direction > 0 and 1 or descriptor.size
-
-        local rest = 1
-        for i = start, reverse_stop, -direction do
-            rest = rest - descriptor[i].factor
-        end
-        if rest > 0 then
-            local total = 0
-            for i = start + direction, stop, direction do
-                total = total + descriptor[i].factor
-            end
-            if total > 0 then
-                local f = rest / total
-                for i = start + direction, stop, direction do
-                    descriptor[i].factor = clamp(descriptor[i].factor * f, 0.01, 0.99)
-                end
-            end
-        end
-    end
-end
-
-function layout_descriptor.object:set_factors(column_descriptor, index, factors, directions)
-    if directions.x ~= 0 then
-        set_factor(self, column_descriptor.index, factors.x, directions.x)
-    end
-    if directions.y ~= 0 then
-        set_factor(column_descriptor, index, factors.y, directions.y)
-    end
-    self.tag:emit_signal("property::tilted_layout_descriptor")
-end
-
-local function normalize_factors(descriptor, size, last_size)
-    if not size or size < 1 then
+local function normalize_factors(descriptor)
+    local size = descriptor.size
+    local last_size = descriptor.last_size or 1
+    if size < 1 then
         return
     end
-    if not last_size or last_size < 1 then
+    if last_size < 1 then
         last_size = 1
     end
 
     local total = 0
 
     for i = 1, size do
-        local item = descriptor[i]
-        if not item then
-            item = {}
-            descriptor[i] = item
+        local item_descriptor = descriptor[i]
+        if not item_descriptor.factor then
+            item_descriptor.factor = 1 / last_size
         end
-        if not item.factor then
-            item.factor = 1 / last_size
-        end
-        total = total + item.factor
+        total = total + item_descriptor.factor
     end
 
     if total > 0 then
         for i = 1, #descriptor do
-            local item = descriptor[i]
-            item.factor = item.factor / total
+            local item_descriptor = descriptor[i]
+            item_descriptor.factor = item_descriptor.factor / total
         end
     end
 end
@@ -132,40 +73,43 @@ function layout_descriptor.update(tag, clients)
         self = setmetatable({ tag = tag }, { __index = layout_descriptor.object })
     end
 
-    local total_count = #clients
-    local primary_count = total_count <= tag.master_count and total_count or tag.master_count
-    local secondary_count = total_count - primary_count
-    local secondary_column_count = secondary_count <= tag.column_count and secondary_count or tag.column_count
-
     local column = 1
     local index = 1
 
-    local function update_next_column_descriptor(size, is_primary)
+    local function update_next_column_descriptor(size)
         local column_descriptor = self[column]
         if not column_descriptor then
             column_descriptor = {}
             self[column] = column_descriptor
         end
-        column_descriptor.is_primary = is_primary
         column_descriptor.index = column
         column_descriptor.from = index
         column_descriptor.to = index + size - 1
-
-        local last_size = column_descriptor.size
+        column_descriptor.last_size = column_descriptor.size
         column_descriptor.size = size
 
-        normalize_factors(column_descriptor, size, last_size)
-
         for i = 1, size do
-            column_descriptor[i].window = clients[column_descriptor.from + i - 1].window
+            local item_descriptor = column_descriptor[i]
+            if not item_descriptor then
+                item_descriptor = {}
+                column_descriptor[i] = item_descriptor
+            end
+            item_descriptor.index = index
+            item_descriptor.window = clients[index].window
+
+            index = index + 1
         end
 
         column = column + 1
-        index = index + size
     end
 
+    local total_count = #clients
+    local primary_count = total_count <= tag.master_count and total_count or tag.master_count
+    local secondary_count = total_count - primary_count
+    local secondary_column_count = secondary_count <= tag.column_count and secondary_count or tag.column_count
+
     if primary_count > 0 then
-        update_next_column_descriptor(primary_count, true)
+        update_next_column_descriptor(primary_count)
     end
 
     if secondary_count > 0 and secondary_column_count > 0 then
@@ -182,13 +126,15 @@ function layout_descriptor.update(tag, clients)
     end
 
     local size = column - 1
-    local last_size = self.size
-
     self.from = size > 0 and 1 or 0
     self.to = size
+    self.last_size = self.size
     self.size = size
 
-    normalize_factors(self, size, last_size)
+    for i = 1, self.size do
+        normalize_factors(self[i])
+    end
+    normalize_factors(self)
 
     if is_new then
         tag.tilted_layout_descriptor = self
